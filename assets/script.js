b3ee80c8423704d9adc0c3f742313cfc9e6a6f52
Desa <eetoppe@gmail.com>
2021-03-07 23:49:44 -0700

    completed HTML, CSS, JS

--- assets/script.js
+++ assets/script.js
@@ -1,107 +1,151 @@
 // Global Variables
-var enter;
-var promptNumber;
-var promptCharacter;
-var promptUpper;
-var promptLower;
+var genBtn = document.querySelector('#generate');
+var lowercaseChar  = 'abcdefghijklmnopqrstuvwxyz';
+var uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '0123456789';
+var specials = '!@#$%^&*()_-+={}[];:`~<,>.?/|'
+var passswordLength;
+var uppercaseCheck;
+var specialCheck;
+var numberCheck;
+var lowercaseCheck
 
-// Password values stored in an array
-// Special characters
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-//Numerals stored in an array
-number = [1,2,3,4,5,6,7,8,9,0];
-// Letters stored in an array
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//Space for uppercase conversion
-space = [];
-
-// Logs responses from prompts for later use
-var responses;
-
-// Converts letters to uppercase
-var toUpper = function(x){
-    return x.toUpperCase();
+// Function asks length of the password
+function checkLength(){
+    passswordLength = prompt('How long would you like your password? Enter 8-128.');
+    if(passswordLength < 8 || !passswordLength || isNaN(passswordLength)|| passswordLength > 128){
+        alert('Password length must be between 8 and 128 characters.')
+        checkLength();
+    }
+    return passswordLength;
 }
 
-// Creates a variable for uppercase conversion
-alpha = letters.map(toUpper);
-
-// Selects the generate button
-var get = document.querySelector('#generate');
-
-// Once the generate button is clicked, the function starts to generate a password
-get.addEventListener('click',function()
-{
-    pw = generatePassword();
-    document.getElementById('password').placeholder = pw;
-})
-
-// Function to generate a password
-function generatePassword(){
-    enter = parseInt(prompt('Enter how many characters for your password, 8-128'));
-    // Makes sure a value is entered
-    if(!enter || enter < 8 || enter > 128 ){
-        alert('Entry must not be blank, and must be 8-128')
+// Function asks if the user wants uppercase
+function checkUppercase(){
+    uppercaseCheck = confirm('Do you want your password to contain uppercase letters? Confirm for yes, cancel for no.');
+    if(uppercaseCheck){
+        uppercaseCheck= true;
+        return uppercaseCheck;
+        console.log('uppercase = true');
     }else{
-        // Continues, and asks the required questions
-        promptNumber = confirm("Will this contain numbers?");
-        promptCharacter = confirm("Will this contain special characters?");
-        promptUpper = confirm("Will this contain uppercase letters?");
-        promptLower = confirm("Will this contain lowercase letters?");
+        uppercaseCheck = false;
+        return uppercaseCheck;
+        console.log('uppercase = false');
     }
-    // Makes sure that options are selected
-    if(!promptLower && !promptCharacter && !promptNumber && !promptUpper){
-        responses = alert('You must choose at least one option!')
+    return uppercaseCheck;
+}
+// Function asks if the user wants lowercase letters
+function checkLowercase(){
+    lowercaseCheck = confirm('Do you want your password to contain lowercase letters? Confirm for yes, cancel for no.');
+    if(lowercaseCheck){
+        lowercaseCheck= true;
+        return lowercaseCheck;
+        console.log('lowercase = true');
+    }else{
+        lowercaseCheck = false;
+        return lowercaseCheck;
+        console.log('lowercase = false');
     }
-    // User selected all options
-    else if(promptUpper && promptNumber && promptLower && promptCharacter){
-        responses = character.concat(number,letters,alpha);
+    return lowercaseCheck;
+}
+// Function asks if user wants numbers
+function checkNumbers(){
+    numberCheck = confirm('Do you want your password to contain numbers? Confirm for yes, cancel for no.');
+    if(numberCheck){
+    numberCheck= true;
+        return numberCheck;
+        console.log('numbercheck = true');
+    }else{
+        numberCheck = false;
+        return numberCheck;
+        console.log('numbercheck = false');
     }
-    //User selected 3 options
-    else if (promptCharacter && promptNumber && promptUppercase) {
-        choices = character.concat(number, alpha);
+    return numberCheck;
+}
+// Function asks if user wants special characters
+function checkSpecial(){
+    specialCheck = confirm('Do you want your password to contain special characters? Confirm for yes, cancel for no.');
+    if(specialCheck){
+    specialCheck= true;
+        return specialCheck;
+        console.log('specialcheck = true');
+    }else{
+        specialCheck = false;
+        return specialCheck;
+        console.log('specialcheck = false');
     }
-    else if (promptCharacter && promptNumber && promptLowercase) {
-        choices = character.concat(number, alpha);
-    }
-    else if (promptCharacter && promptLowercase && promptUppercase) {
-        choices = character.concat(alpha, letters);
-    }
-    else if (promptNumber && promptLowercase && promptUppercase) {
-        choices = number.concat(letters, alpha2);
-    }
-    //User selected 2 options
-    else if (promptCharacter && promptNumber) {
-        choices = character.concat(number);
+    return specialCheck;
+}
 
-    } else if (promptCharacter && promptLowercase) {
-        choices = character.concat(letters);
+// Function uses responses to generate a password
+function generatePassword(){
+    checkLength();
+    checkNumbers();
+    checkSpecial();
+    checkLowercase();
+    checkUppercase();
 
-    } else if (promptCharacter && promptUppercase) {
-        choices = character.concat(alpha);
-    }
-    else if (promptLowercase && promptNumber) {
-        choices = letters.concat(number);
+    var characters;
+    var password = '';
+    if (uppercaseCheck && lowercaseCheck && numberCheck && specialCheck){
+        characters += uppercaseChar + lowercaseChar + numbers + specials;
 
-    } else if (promptLowercase && promptUppercase) {
-        choices = letters.concat(alpha);
+    }else if (uppercaseCheck && lowercaseCheck && numberCheck){
+        characters += uppercaseChar + lowercaseChar + numbers;
 
-    } else if (promptNumber && promptUppercase) {
-        choices = number.concat(alpha);
-    }
-    // User selected one option
-    else if (promptCharacter) {
-        choices = character;
-    }
-    else if (promptNumber) {
-        choices = number;
-    }
-    else if (promptLowercase) {
-        choices = letters;
-    }
-    // Used space variable to fill uppercase conversion
-    else if (promptUppercase) {
-        choices = space.concat(alpha);
-    };
+    }else if (uppercaseCheck && lowercaseCheck && specialCheck){
+        characters += uppercaseChar + lowercaseChar + specials;
+
+    }else if (uppercaseCheck && numberCheck && specialCheck){
+        characters += uppercaseChar + numbers + specials;
+
+    }else if (lowercaseCheck && numberCheck && specialCheck){
+        characters += lowercaseChar + numbers + specials;
+
+    }else if (lowercaseCheck && numberCheck){
+        characters += lowercaseChar + numbers;
     
-}   
+    }else if (lowercaseCheck && uppercaseCheck){
+        characters += lowercaseChar + uppercaseChar;
+
+    }else if (lowercaseCheck && specialCheck){
+        characters += lowercaseChar + specials;
+    }else if (uppercaseCheck && numberCheck){
+        characters += uppercaseChar + numbers;
+
+    }else if (uppercaseCheck && specialCheck){
+        characters += uppercaseChar + specials;
+
+    }else if (specialCheck && numberCheck){
+        characters += specials + numbers;
+
+    }else if (specialCheck){
+        characters === specials;
+    }else if (numberCheck){
+        characters === numbers;
+
+    }else if (lowercaseCheck){
+        characters === lowercaseChar;
+
+    }else if (uppercaseChar){
+        characters === uppercaseChar;
+
+    }
+
+    for(var i = 0; i < passswordLength; i++){
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return password;
+
+}
+
+// Writes the password to the Password ID
+function writePassword(){
+    var password1 = '';
+    password1 = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password1;
+}
+
+// Makes the generate button start the process
+genBtn.addEventListener('click', writePassword);
